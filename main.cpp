e9ec48996ca7c466260bbc3228288c290363781a
Yohannes  Kassa <83584566+yohannes-jo@users.noreply.github.com>
2021-09-06 07:36:34 +0000

    comments

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/**
+ * SecE-SWEG2012Batch-G5
+ * 
+ * Group Members
+ * 1. Milkaai Getachew 0478/12
+ * 2. Naol Tamrat 0512/12
+ * 3. Tselote Yonas 1052/12
+ * 4. Wengelawit Getachew 1054/12
+ * 5. Yohannes Kassa 1063/12
+ * 
+ */
 #include "include/MainMenu.h"
 
 int main()
